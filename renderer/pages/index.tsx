ca707039a493b7568aa1b385fc566f8cbe5b7cd9
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2024-02-02 00:52:06 +0100

    Add working focus timer with tag selection

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,40 +1,155 @@
 import type { NextPage } from "next";
-import { Center, Divider, Stack, Title } from "@mantine/core";
-import pkg from "../../package.json";
+import {
+  Button,
+  Center,
+  Combobox,
+  Container,
+  Input,
+  InputBase,
+  RingProgress,
+  Slider,
+  SliderProps,
+  Stack,
+  Text,
+  Title,
+  useCombobox,
+} from "@mantine/core";
 import DefaultLayout from "@components/layout/DefaultLayout";
-import { APP_NAME } from "utils/constants";
+import { useInputState } from "@mantine/hooks";
+import { DEFAULT_FOCUS_SETTINGS } from "@utils/constants";
+import { IconTag } from "@tabler/icons-react";
 
 /**
  *
- * The main page displaying essential information about the application and providing
- * routes to the user.
- *
- * Currently only displaying a logo with a version string.
- *
  * @author Tim Deres <derechtenap>
- * @todo Enhance user experience by expanding content beyond just the logo display.
  *
- * @returns {JSX.Element} JSX element representing the main page content.
+ * @returns {JSX.Element}
  *
  */
 const IndexPage: NextPage = (): JSX.Element => {
-  const APP_VERSION = pkg.version;
+  const [focusMinutes, setMinutes] = useInputState<number>(
+    DEFAULT_FOCUS_SETTINGS.TIMER.DEFAULT_MINS
+  );
+  const [focusTag, setFocusTag] = useInputState<string>("");
 
-  /*
-   *
-   * TODO: Enhance user experience by expanding content beyond just the
-   * logo display.
-   *
-   */
+  const combobox = useCombobox({
+    onDropdownClose: () => combobox.resetSelectedOption(),
+  });
+
+  const sliderOptions: SliderProps["marks"] = [
+    {
+      label: "5 min.",
+      value: 5,
+    },
+    // For min < 120 min... 20min steps
+    ...Array.from({ length: 6 }, (_, index) => ({
+      label: `${(index + 1) * 20} min.`,
+      value: (index + 1) * 20,
+    })),
+  ];
+
+  const options = DEFAULT_FOCUS_SETTINGS.DEFAULT_TAGS.map((tag) => (
+    <Combobox.Option value={tag} key={tag}>
+      {tag}
+    </Combobox.Option>
+  ));
+
+  const startFocus = () => {
+    const focusSessionData: FocusSession = {
+      startedAt: Date.now(),
+      minutes: focusMinutes,
+      tag: focusTag,
+      isAborted: false,
+      isCompleted: false,
+    };
+
+    // TODO: Add routing to session page
+    console.info(focusSessionData);
+  };
+
   return (
     <DefaultLayout>
       <Center h="100%">
-        <Stack>
-          <Title tt="uppercase" fs="italic" size={100} lh={0.8}>
-            {APP_NAME}
-          </Title>
-          <Divider label={APP_VERSION} labelPosition="right" />
-        </Stack>
+        <Container w="100%" maw={600}>
+          <Stack align="center" gap="xl" w="100%">
+            <Title c="dark.1" fz="lg" fw="normal" ta="center">
+              Hello, {"{username}"}! You focused{" "}
+              <Text component="span" fw="bold">
+                0 minutes
+              </Text>{" "}
+              today.
+            </Title>
+            <RingProgress
+              label={
+                <Text ta="center">
+                  Let's focus for{" "}
+                  <Text
+                    fz="sm"
+                    c="green"
+                    fw={900}
+                    component="span"
+                    truncate="end"
+                  >
+                    {focusMinutes} minutes
+                  </Text>{" "}
+                  together!
+                </Text>
+              }
+              size={300}
+              thickness={15}
+              roundCaps
+              sections={[{ color: "green", value: (focusMinutes / 120) * 100 }]}
+              mb={0}
+            />
+            <Slider
+              w="100%"
+              mb="md"
+              defaultValue={DEFAULT_FOCUS_SETTINGS.TIMER.DEFAULT_MINS}
+              labelTransitionProps={{
+                transition: "pop",
+                duration: 200,
+              }}
+              showLabelOnHover={false}
+              marks={sliderOptions}
+              min={DEFAULT_FOCUS_SETTINGS.TIMER.MIN_MINS}
+              max={DEFAULT_FOCUS_SETTINGS.TIMER.MAX_MINS}
+              onChange={(minutes) => setMinutes(minutes)}
+            />
+            <Combobox
+              store={combobox}
+              onOptionSubmit={(tag) => {
+                setFocusTag(tag);
+                combobox.closeDropdown();
+              }}
+            >
+              <Combobox.Target>
+                <InputBase
+                  leftSection={<IconTag size="16px" />}
+                  component="button"
+                  type="button"
+                  pointer
+                  rightSection={<Combobox.Chevron />}
+                  rightSectionPointerEvents="none"
+                  onClick={() => combobox.toggleDropdown()}
+                >
+                  {focusTag || <Input.Placeholder>Pick Tag</Input.Placeholder>}
+                </InputBase>
+              </Combobox.Target>
+              <Combobox.Dropdown>
+                <Combobox.Options>{options}</Combobox.Options>
+              </Combobox.Dropdown>
+            </Combobox>
+            <Button
+              autoContrast
+              mx="auto"
+              variant="gradient"
+              gradient={{ from: "green", to: "blue", deg: 140 }}
+              onClick={() => startFocus()}
+            >
+              Let's Start!
+            </Button>
+          </Stack>
+        </Container>
       </Center>
     </DefaultLayout>
   );
--- renderer/utils/constants.ts
+++ renderer/utils/constants.ts
@@ -2,3 +2,13 @@ import pkg from "../../package.json";
 
 export const APP_NAME = pkg.productName;
 export const APP_VERSION = pkg.version;
+
+export const DEFAULT_FOCUS_SETTINGS = {
+  TIMER: {
+    // Minutes
+    DEFAULT_MINS: 30,
+    MIN_MINS: 5,
+    MAX_MINS: 120,
+  },
+  DEFAULT_TAGS: ["Study", "Work", "Meditation", "Exercise", "Break"],
+} as const;
